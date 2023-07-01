813734bdc42316b776f3f5a06b3b3815bc96e64d
Like <like@ekil.sh>
2023-07-01 21:00:33 +0800

    .

--- app/AuthProvider.tsx
+++ app/AuthProvider.tsx
@@ -1,19 +1,21 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useLayoutEffect, useState } from 'react'
 
 export const AuthContext = createContext({
-    isSignedIn: localStorage.getItem('isSignedIn') === 'true',
+    isSignedIn: false,
     setIsSignedIn: (isSignedIn: boolean) => {},
 })
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [isSignedIn, setter] = useState(
-        localStorage.getItem('isSignedIn') === 'true',
-    )
+    const [isSignedIn, setter] = useState<boolean>(false)
 
     function setIsSignedIn(isSignedIn: boolean) {
         localStorage.setItem('isSignedIn', isSignedIn.toString())
         setter(isSignedIn)
     }
+
+    useLayoutEffect(() => {
+        setter(localStorage.getItem('isSignedIn') === 'true')
+    }, [])
 
     return (
         <AuthContext.Provider value={{ isSignedIn, setIsSignedIn }}>
